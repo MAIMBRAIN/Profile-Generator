d88ec82893d8b09cb1ff40dd03f74d34ca16329a
Avery Stahl <averystahl@hotmail.com>
2019-12-19 14:32:15 -0500

    Imported generateHTML.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // Get the nodes
 var fs = require("fs");
 var inquirer = require("inquirer");
+var page = require("./generateHTML")
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        choices: ["Red", "Blue", "Green", "Pink"],
+        name: "color"
+    }
+]).then(function(data)
+{
+    fs.writeFile("profile.pdf", page.generateHTML);
+})
