f115b1e8db06b8b1b98aaff104342bac1c4d7677
Avery Stahl <averystahl@hotmail.com>
2020-01-15 23:08:23 -0500

    Need to work on wrapperbackground error...

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios")
-const page = require("./generateHTML");
+const generateHTML = require("./generateHTML");
+const electron = require("electron")
+const convertFactory = require("electron-html-to")
 
+const conversion = convertFactory({converterPath: convertFactory.converters.PDF});
 
+// Ask user their username and favorite color
 inquirer.prompt([
     {
         type: "input",
@@ -17,13 +21,56 @@ inquirer.prompt([
         choices: ["Red", "Blue", "Green", "Pink"],
         name: "color"
     }
-]).then(function({username})
+]).then(function(res)
 {
-    const queryURL = `https://api.github.com/users/${username}`
+    const profileURL = `https://api.github.com/users/${res.username}`;
+    const repoURL = `https://api.github.com/users/${res.username}/repos`;
     
-    axios.get(queryURL).then(res => 
+    // Profile API Request
+    axios.get(profileURL).then(userResult => 
+    {
+        const profileData = userResult.data;
+
+        // Log API Data
+        console.log(profileData)
+
+        // Repo API request for stargazer count
+        axios.get(repoURL).then(response =>
         {
-            fs.writeFile("profile.pdf", page.generateHTML);
+            const repos = response.data;
+            let totalStars = 0;
+            repos.forEach(repo => totalStars += repo.stargazers_count);
+
+            // Generate the HTML
+            let data = {
+                name: profileData.name,
+                color: res.color,
+                avatar_url: profileData.avatar_url,
+                location: profileData.location,
+                html_url: profileData.html_url,
+                followers: profileData.followers,
+                following: profileData.following,
+                public_repos: profileData.public_repos,
+                bio: profileData.bio,
+                stars: totalStars
+            }
+            let genHTML = generateHTML(data);
+            fs.writeFile(`${profileData.name}.html`, genHTML, function (err) {
+                if (err) throw err;
+            })
         })
-    
-})
+
+        //     // Convert HTML into PDF
+        //     conversion({html: genHTML}, function(err, result)
+        //     {
+        //         if(err)
+        //         {
+        //             return console.error(err);
+        //         }
+
+        //         result.stream.pipe(fs.createWriteStream(`${profileData.name}.pdf`));
+        //         conversion.kill();
+        //     });
+        // });
+    });
+});
